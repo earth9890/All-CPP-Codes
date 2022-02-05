5b40efcb239c3db1c2224071637cb41c14627b3c
harishsganndhi123 <harishsugandhi9890@gmail.com>
2022-02-05 23:32:13 +0530

    CodeChef and CodeForces

---
+++ BinaryBaseBasics.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, k;
+        string s;
+        cin >> n >> k;
+        cin >> s;
+    }
+}
