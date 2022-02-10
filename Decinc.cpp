7173b45d2cb8668482860b45bdd95bc515e6f6e1
harishsganndhi123 <harishsugandhi9890@gmail.com>
2022-02-10 13:47:44 +0530

    Codechef

---
+++ Decinc.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    if (n % 4 == 0)
+        cout << ++n;
+    else
+        cout << --n;
+
+    return 0;
+}
