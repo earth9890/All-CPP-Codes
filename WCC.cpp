fd3338f299f6482029981ef3001f8dc166fdfcdd
harishsganndhi123 <harishsugandhi9890@gmail.com>
2022-02-11 23:49:38 +0530

    CodeChef and CodeForces

---
+++ WCC.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int maint()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x;
+        cin >> x;
+        string s;
+        cin >> s;
+    }
+}
