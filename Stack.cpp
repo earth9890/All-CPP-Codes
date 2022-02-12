d5f1a5ec6dd6d4f4652fa62bf23dfbf5150b1ef2
harishsganndhi123 <harishsugandhi9890@gmail.com>
2022-02-12 17:48:30 +0530

    Stack CodeChef

--- Stack.cpp
+++ Stack.cpp
@@ -3,13 +3,81 @@ using namespace std;
 #define MAX 1000
 class Stack
 {
-    int top;
 
 public:
+    int top;
     int a[MAX];
     Stack()
     {
         top = -1;
     }
-    bool
+    void push(int y);
+    int pop();
+    bool empty();
+    bool overflow();
 };
+bool Stack::empty()
+{
+    if (top == -1)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+bool Stack::overflow()
+{
+    if (top >= MAX)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void Stack ::push(int x)
+{
+    if (!overflow())
+    {
+
+        a[++top] = x;
+    }
+    else
+    {
+        cout << "Stack is Full" << endl;
+    }
+}
+int Stack ::pop()
+{
+    int data;
+    if (!empty())
+    {
+        data = a[top];
+        top = top - 1;
+        return data;
+    }
+    else
+    {
+        cout << "Stack is Empty" << endl;
+    }
+}
+
+int main()
+{
+    Stack s;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    s.push(40);
+    cout << " Popped " << s.pop() << endl;
+    int i = s.top;
+    while (!s.empty())
+    {
+        int data = s.pop();
+        cout << "Element of Stack " << i-- << ":" << data << endl;
+    }
+}
--- WCC.cpp
+++ WCC.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-int maint()
+int main()
 {
     int t;
     cin >> t;
@@ -10,5 +10,16 @@ int maint()
         cin >> x;
         string s;
         cin >> s;
+        int countDraw = 0, countCarslen, countChef;
+        int cou
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (s[i] == 'C');
+            {
+               
+            }
+        }
+        cout << countDraw;
     }
+    return 0;
 }
